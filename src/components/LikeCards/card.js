c624a4a0935f76e1f8c23e9b29eefb4dac40f895
Jerry Pallath <jerrypallath@jerrys-mbp.lan>
2023-12-20 16:47:25 -0500

    updates to likes

--- src/components/LikeCards/card.js
+++ src/components/LikeCards/card.js
@@ -3,10 +3,6 @@ import { updateLike } from "../../features/likes";
 import styles from "./likeCards.module.scss";
 export const Card = ({ data }) => {
   const dispatch = useDispatch();
-  // const openLink = () => {
-  //   window.open(data.link, "_blank", "noopener noreferrer");
-  //   window.focus();
-  // };
   const updateLikesContainer = () => {
     dispatch(
       updateLike({
@@ -17,28 +13,21 @@ export const Card = ({ data }) => {
       })
     );
   };
-  const overlay = () => {
-    return styles.overlay;
-  };
+
   return (
-    <div className={styles.likeCard} onClick={updateLikesContainer}>
-      <div className={styles.baseInformation}>
-        <img src={data.image} alt="" />
-        <div className={styles.text}>
-          <h3>{data.title}</h3>
-          {/* <p>{data.description}</p> */}
-        </div>
-      </div>
-      <div className={overlay()} onClick={updateLikesContainer}>
-        <div className={styles.overlayData}>
-          <div className={styles.noData}></div>
-          <div className={styles.data}>
-            <h1>{data.title}</h1>
-            <p>{data.description}</p>
+    <div className={styles.likeCard}>
+      <div className={styles.likeCard1} onClick={updateLikesContainer}>
+        <div className={styles.baseInformation}>
+          <div className={styles.imageContainer}>
+            <img src={data.image} alt="" />
           </div>
-          <div className="likeButtons">{/* <button>Discuss</button> */}</div>
+          <div className={styles.text}>
+            <h3>{data.title}</h3>
+          </div>
         </div>
       </div>
+      <div className={styles.likeCard2}></div>
+      <div className={styles.likeCard3}></div>
     </div>
   );
 };
